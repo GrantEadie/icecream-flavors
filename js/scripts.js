89da121b8ae66295b467cf153391d1da24170dd1
Grant Eadie <grantleadie@gmail.com>
2020-08-17 13:41:16 -0700

    simply list flavors onto page not needing HTML input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,7 @@
 $(document).ready(function() {
-  let array0 = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+  let flavors = ["chocolate", "vanilla", "cookie dough", "olives"];
 
-  let userInput;
-
-  $("#formOne").submit(function(event) {
-    array0.forEach(function(element) {
-
-      userInput = $("input#" + element).val();
-      $("." + element).text(userInput);
-    });
-    
-    $("#story").show();
-
-    event.preventDefault();
+  flavors.forEach(function(flavor) {
+    $("#icecream").append("<li>" + flavor + "</li>");
   });
 });
-
-
-// this also functions -- keep in mind that forEach is the same as the num++ idea
-
-      //let array1 = [];
-      //let i = 0;
-      //array1.push($("input#" + element).val());
-      //$("." + element).text(array1[i]);
-      //i++
