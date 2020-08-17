ad089e0d667194b9f81ecbbd3dbd7e2afd560339
Grant Eadie <grantleadie@gmail.com>
2020-08-17 12:03:13 -0700

    will take each answer and populate array to reflect in the mad lib answers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,45 @@
 $(document).ready(function() {
-  // let array0 = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
-  // let array1 = [];
-  // let array2 = [];
+  let array0 = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+  let array1 = [];
+  let phrase;
+
+
 
   $("#formOne").submit(function(event) {
-    // array0.forEach(function(element) {
-    //   array1.push( $("input#" + array0[element] + '"').val())
-    // }
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
-    const inputArray = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
-
-    // array1.forEach(function(element) {
-    // $("." + array0[element] + '"').text(array1[element]);
-    // }
-
-    inputArray.forEach(function(element){
-      $("#display-input").append("<li>" + element + "</li>";)
+    array0.forEach(function(element) {
+      array1.push($("input#" + element).val());
     });
+    
+    let num = 0;
+
+    array0.forEach(function(element) {
+      alert(array1[num]);
+      $("." + element).text(array1[num]);
+      num++;
+    });
+
+    
+
+
+
+  //   array0.forEach(function(element) {
+  //     phrase = $('"input#' + element + '"').val();
+  //     alert(phrase);
+  //     array1.push(phrase);
+  //   });
+
+    // const person1Input = $("input#person1").val();
+    // const person2Input = $("input#person2").val();
+    // const animalInput= $("input#animal").val();
+    // const exclamationInput = $("input#exclamation").val();
+    // const verbInput = $("input#verb").val();
+    // const nounInput = $("input#noun").val();// 
+    // array1.forEach(function(element) {
+    //   phrase = '$(".' + array0[element] + '").text(array1[element])'
+    //   alert(phrase);
+    //   $("." + array0[element] + '"').text(element);
+    // });
+    // // // 
 
     // $(".person1").text(person1Input);
     // $(".person2").text(person2Input);
